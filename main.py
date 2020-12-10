6d7e928e7da0b24a0731de9b0984b35e034016f8
Akshat Garg <gargakshat99@gmail.com>
2020-12-10 23:23:04 +0530

    created objects

--- main.py
+++ main.py
@@ -318,3 +318,13 @@ class GradCam():
 
         return cam
 
+if __name__ == '__main__':
+    target_example = 0  # Snake
+    (original_image, prep_img, target_class, file_name_to_export, pretrained_model) = get_example_params(target_example)
+    # Grad cam
+    grad_cam = GradCam(pretrained_model, target_layer=11)
+    # Generate cam mask
+    cam = grad_cam.generate_cam(prep_img)
+    # Save mask
+    save_class_activation_images(original_image, cam, file_name_to_export)
+    print('Grad cam completed')
